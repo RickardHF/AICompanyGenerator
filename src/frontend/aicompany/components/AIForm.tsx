e78c79c9ffcd431b5d76618ff107b8e8357d793d
Rickard Hole Falck <rickardhole.falck@gmail.com>
2023-07-10 22:46:41 +0200

    Made api calls from react app

--- src/frontend/aicompany/components/AIForm.tsx
+++ src/frontend/aicompany/components/AIForm.tsx
@@ -2,24 +2,53 @@
 
 import React, { useState } from "react";
 import "./AIForm.css";
-import getCompanyData from "../pages/api/getCompanyData";
+import { getCompanyColors, getCompanyName, getCompanySlogan } from "@/pages/api/openaiCommunicator";
 
 function AIForm() {
     const [description, setDescription] = useState("");
 
-    async function generate(e) {
+    const [name, setName] = useState("");
+    const [slogan, setSlogan] = useState("");
+    const [colors, setColors] = useState("");
+
+    async function generate(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log(description);
-        const response = await getCompanyData({input: description});
-        console.log(response);
+        try {
+            const name = await getCompanyName(description);
+            const slogan = await getCompanySlogan(name, description);
+            const colors = await getCompanyColors(description);
+            console.log(name);
+            console.log(slogan);
+            console.log(colors);
+
+            setName(name);
+            setSlogan(slogan);
+            setColors(colors);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
-        <div>
-            <form onSubmit={generate} className='flex w-full'>
+        <div className="w-full">
+            <form onSubmit={generate} className='flex w-full ver-flex'>
                 <textarea id="description" placeholder='Describe your company...' value={description} onChange={(event) => setDescription(event.target.value)} />
                 <button type="submit">Generate</button>
             </form>
+            <div className='flex w-full ver-flex'>
+                <div className='flex w-full ver-flex'>
+                    <h2>Company Name</h2>
+                    <p>{name}</p>
+
+                    <h2>Company Slogan</h2>
+                    <p>{slogan}</p>
+
+                    <h2>Company Colors</h2>
+                    <p>{colors}</p>
+
+                </div>
+
+            </div>
         </div>
     );
 }
---
+++ src/frontend/aicompany/pages/api/openaiCommunicator.tsx
@@ -0,0 +1,44 @@
+const {Configuration, OpenAIApi} = require('openai');
+
+const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    organization: process.env.NEXT_PUBLIC_OPENAI_ORGANIZATION_ID
+});
+
+const openai = new OpenAIApi(configuration);
+
+export async function getCompanyName(description: string) {
+    const gptResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            "role": "system",
+            "content": `Create a name for a company based on the following description:\n ${description}`
+        }]
+    });
+
+    return gptResponse.data.choices[0].message.content;
+}
+
+export async function getCompanySlogan(name: string, description: string) {
+    const gptResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            "role": "system",
+            "content": `Create a slogan for a company called ${name} based on the following description:\n ${description}`
+        }]
+    });
+
+    return gptResponse.data.choices[0].message.content;
+}
+
+export async function getCompanyColors(description: string) {
+    const gptResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            "role": "system",
+            "content": `Create a color scheme for a company, the answer should only contain two hex-codes sepperated by a comma, based on the following description:\n ${description}`
+        }]
+    });
+
+    return gptResponse.data.choices[0].message.content;
+}
