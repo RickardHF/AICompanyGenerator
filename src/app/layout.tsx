1a1096a8bbab9e39b7bb739560a5d38f77f1da6f
Rickard Hole Falck <rickardhole.falck@gmail.com>
2023-07-26 21:46:59 +0200

    a bit better seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'AICompany',
-  description: 'AI Company Generator',
+  title: 'AI Company Name and Slogan Generator',
+  description: 'Our AI-powered company name and slogan generator will help you find the perfect name for your business. All you need to do is describe your business and the generator takes it from there',
 }
 
 export default function RootLayout({
