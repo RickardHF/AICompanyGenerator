702be47ea8e6fa9442b7b764697eb3fa03ce0da9
Rickard Hole Falck <rickard.hole.falck@solidify.dev>
2023-08-09 09:33:40 +0200

    trying to make it work

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,14 +7,14 @@ export default function Header() {
     return (<header className="flex w-full hor-flex justify-center items-center">
         <div className="content-container flex w-full hor-flex space-between items-center">
             <a
-                className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 link-item shadow-color-two"
+                className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 link-item shadow-color-one"
                 href="/"
                 rel="noopener noreferrer"
             >
                 <h2>{link_names("home")}</h2>
             </a>
             <a
-                className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 link-item shadow-color-two"
+                className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 link-item shadow-color-one"
                 href="/about"
                 rel="noopener noreferrer"
             >
